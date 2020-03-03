1748c71178957f654634144cb5a24ae7f3882699
Jordan <jordan.maisonneuve@ucalgary.ca>
2020-03-03 05:52:58 -0700

    added back console log messages for chat app

--- Assignment3/chat-app/index.js
+++ Assignment3/chat-app/index.js
@@ -7,14 +7,19 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
   socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
     io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
 
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
-
+//to be replaced with var port #?
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
