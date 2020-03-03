6e8368c85a7672af2b2db91edec680a6f112c2ee
Jordan <jordan.maisonneuve@ucalgary.ca>
2020-03-03 06:23:59 -0700

    minor changes

--- Assignment3/chat-app/index.js
+++ Assignment3/chat-app/index.js
@@ -7,19 +7,22 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+
   console.log('a user connected');
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('a user disconnected');
   });
 });
 
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
-//to be replaced with var port #?
+//to be replaced with var port # which is website url??
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
