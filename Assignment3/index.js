53f3ad76858bfa72d01547bedbdb261b769fdc23
Jordan <jordan.maisonneuve@ucalgary.ca>
2020-03-08 17:36:23 -0600

    Added early user list display

--- Assignment3/index.js
+++ Assignment3/index.js
@@ -1,51 +1,89 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-let userArray = new Array();
 
+
+let firstConnection = true; //for username array.. tbd
+let userArray = new Array();
+let chatLog = new Array(); //todo: put chat log on scrn when user reconnects.
+
+//gets the html file
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
 
+io.on('connection', function(socket) {
   console.log('a user connected');
 
-  var usrName;
+  //restore chat log when user connects/reconnects
+  chatLog.forEach((item, i) => {
+    io.emit('chat message', item);
+  });
+
+  //dealing with username stuff -- put in its own function?
+  console.log('the current user array: ' + userArray.toString());
+  var usrName = 'user0'; //initial user name
+  if (firstConnection){
+      userArray.push(usrName);
+      //console.log('sending user list update.');
+      //io.emit('chat message', 'sending user list update.');
+      //io.emit('user list update', userArray);
+      firstConnection = false;
+  }
+
   userArray.forEach((item, i) => {
-    console.log(item);
+    console.log('in for loop item: ' + item);
+
     if (usrName === item) {
       usrName = 'user' + i;
       var usrNameMsg = 'Your username is: ' + usrName;
       console.log(usrNameMsg);
-
+      userArray.push(usrName);
+      //io.emit('user list update', usrName);
     }
   });
-
   io.emit('chat message', 'Your username is: ' + usrName);
-  userArray.push(usrName)
+  //end username stuff
 
   socket.on('chat message', function(msg) {
 
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    //display/print the message. -- possibly put in a function.
+    console.log('message: '+ Date.now() + ' ' + msg);
+    io.emit('chat message', Date.now() + ' ' + msg);
+    chatLog.push(Date.now() + ' ' + msg); //tbd format date?
 
     //chat functions
     if (msg === 'printUsrArray') {
-      userArray.forEach(element => console.log(element));
-    } else if (msg.includes("/nick")) {
+
+      console.log(userArray.toString());
+      io.emit('chat message', userArray.toString());
+
+    } else if (msg.includes("/nick")) { //todo: need a better condition.
+
+      console.log('nickname change command: ' + msg);
+      io.emit('chat message', 'nickname change command');
+      //todo: ensure nickname is not blank & is not already taken
+      var newNickName = msg.slice(6);
+      console.log('new nickname is: ' + newNickName);
 
     } else if (msg.includes("/nickcolor")) {
 
+      console.log('nickname color change command');
+      io.emit('chat message', 'nickname color change command');
+      var nickColorRRGGBB = msg.slice(11);
+      //todo: ensure nickColorRRGGBB is valid
+      console.log('nickname color RRGGBB is: ' + nickColorRRGGBB);
     }
-
-
-
   });
 
-  //display which user disconnected?>
+
+  //todo?: display which user disconnected?
   socket.on('disconnect', function() {
     console.log('a user disconnected');
+    io.emit('chat message', 'A user disconnected.')
+    //find out which user disconnected to remove them from the usr array list.
+
   });
 });
 
@@ -54,7 +92,6 @@ io.emit('some event', {
   otherProperty: 'other value'
 }); // This will emit the event to all connected sockets
 
-//to be replaced with var port # which is website url??
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
